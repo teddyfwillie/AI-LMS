0bd6fad53b737063ef81e81b98bfcb6bb9f706b2
teddyfwillie <willieteddyf@gmail.com>
2025-06-27 00:52:05 +0530

    Modified the payment component

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,6 @@
-const config = {
-  plugins: ["@tailwindcss/postcss", "autoprefixer"],
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
-
-export default config;
